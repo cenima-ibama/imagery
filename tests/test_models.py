8840cb12a2fdf9410c29cf12b0be542adfd8cac4
Wille Marcel <wille@wille.blog.br>
2015-04-20 14:48:41 -0300

    improve tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,8 +9,8 @@ from ..models import Scene, Image
 
 class TestScene(TestCase):
 
-    def test_creation(self):
-        scene = Scene.objects.create(
+    def setUp(self):
+        self.scene = Scene.objects.create(
             path='001',
             row='001',
             sat='LC8',
@@ -20,8 +20,9 @@ class TestScene(TestCase):
             status='downloading'
             )
 
-        self.assertEqual(scene.__str__(), 'LC8 001-001 01/01/15')
-        self.assertEqual(scene.day(), '001')
+    def test_creation(self):
+        self.assertEqual(self.scene.__str__(), 'LC8 001-001 01/01/15')
+        self.assertEqual(self.scene.day(), '001')
 
         Scene.objects.create(
             path='001',
@@ -32,17 +33,18 @@ class TestScene(TestCase):
             status='downloading'
             )
 
+        self.assertEqual(Scene.objects.all().count(), 2)
+
+    def test_validation(self):
         with self.assertRaises(ValidationError):
             Scene.objects.create(
                 path='001',
                 row='001',
                 sat='LC8',
-                date=date(2015, 1, 17),
-                name='LC80010012015017LGN00',
+                date=date(2015, 1, 1),
+                name='LC80010012015001LGN00',
                 status='downloading'
                 )
-
-        self.assertEqual(Scene.objects.all().count(), 2)
 
 
 class TestImage(TestCase):
@@ -58,15 +60,30 @@ class TestImage(TestCase):
             status='downloading'
             )
 
-    def test_creation(self):
-        image = Image.objects.create(
+        self.image = Image.objects.create(
             name='LC80010012015001LGN00_B4.TIF',
             type='B4',
             scene=self.scene
             )
 
-        self.assertEqual(image.__str__(), 'LC80010012015001LGN00_B4.TIF')
-        self.assertEqual(image.path(),
+    def test_creation(self):
+        self.assertEqual(self.image.__str__(), 'LC80010012015001LGN00_B4.TIF')
+        self.assertEqual(self.image.path(),
             'LC80010012015001LGN00/LC80010012015001LGN00_B4.TIF')
-        self.assertEqual(Image.objects.all().count(), 1)
-        self.assertEqual(self.scene.images().count(), 1)
+
+        Image.objects.create(
+            name='LC80010012015001LGN00_B5.TIF',
+            type='B5',
+            scene=self.scene
+            )
+
+        self.assertEqual(Image.objects.all().count(), 2)
+        self.assertEqual(self.scene.images().count(), 2)
+
+    def test_validation(self):
+        with self.assertRaises(ValidationError):
+            Image.objects.create(
+                name='LC80010012015001LGN00_B4.TIF',
+                type='B4',
+                scene=self.scene
+                )
