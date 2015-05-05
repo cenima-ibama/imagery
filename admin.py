a464964d897fafadf5d0ad23b64c6a3a18eeae44
Wille Marcel <wille.malheiro@ibama.gov.br>
2015-05-05 11:31:21 -0300

    add ScheduledDownload model to admin

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Scene, Image
+from .models import Scene, Image, ScheduledDownload
 
 admin.site.register(Scene, admin.ModelAdmin)
 admin.site.register(Image, admin.ModelAdmin)
+admin.site.register(ScheduledDownload, admin.ModelAdmin)
