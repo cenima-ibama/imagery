52ad3b97f76758b0d55c8f7d9c8230c6f7c00358
Wille Marcel <wille.malheiro@ibama.gov.br>
2015-06-02 14:55:39 -0300

    populates geom and cloud_rate fields when calling inspect_dir function

--- tasks.py
+++ tasks.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from os import listdir
 
+from django.contrib.gis.geos import Polygon
+
 from .models import Scene, Image, ScheduledDownload
-from .utils import calendar_date
+from .utils import calendar_date, get_bounds, get_cloud_rate
 
 
 def download_all():
@@ -21,11 +23,24 @@ def inspect_dir(dir):
     in that dir to create the Image objects in the database.
     """
     scene_name = dir.split('/')[-1]
+
+    try:
+        geom = Polygon(get_bounds(scene_name))
+    except IndexError:
+        geom = None
+
+    try:
+        cloud_rate = get_cloud_rate(scene_name)
+    except FileNotFoundError:
+        cloud_rate = None
+
     scene = Scene.objects.create(
         sat='L' + scene_name[2],
         path=scene_name[3:6],
         row=scene_name[6:9],
         date=calendar_date(scene_name[9:13], scene_name[13:16]),
+        geom=geom,
+        cloud_rate=cloud_rate,
         name=scene_name,
         status='processed'
         )
