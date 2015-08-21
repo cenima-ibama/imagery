e1fc77cc15c650283fd391ce0459ab3a6316d42f
Wille Marcel <wille.malheiro@ibama.gov.br>
2015-08-21 09:57:54 -0300

    improve download command of imagery app

--- management/commands/download.py
+++ management/commands/download.py
@@ -3,18 +3,14 @@ import sys
 
 from django.core.management.base import BaseCommand
 
-from ...models import Scene, ScheduledDownload
+from ...tasks import download_all, process_all
 
 
 class Command(BaseCommand):
     help = """Download and process all the new Scenes."""
 
     def handle(self, *args, **options):
-        for sd in ScheduledDownload.objects.all():
-            sd.download_new_scene()
-            sd.check_last_scene()
-
-        for scene in Scene.objects.filter(status='downloaded'):
-            scene.process()
+        download_all()
+        process_all()
 
         sys.exit(0)
