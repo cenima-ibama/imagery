4cae03ed906563dd529c98ed9ff385e08b16e07b
Wille Marcel <wille.malheiro@ibama.gov.br>
2015-12-08 18:25:00 -0200

    change filter JS code to imagery app

---
+++ static/js/imagery.js
@@ -0,0 +1,41 @@
+/* Project specific Javascript goes here. */
+$(document).ready(function(){
+  $("#startDate").on("change",function(){
+    $("#endDate").datepicker('setStartDate',$(this).val());
+  });
+
+  $("#cloudRange").slider();
+
+  $("#search").on("click", function(){
+    var url = "//" + window.location.host + window.location.pathname + "?";
+
+    var scene = $("#scene").val();
+    var path = $("#path").val();
+    var row = $("#row").val();
+    var sat = $("#sat").val();
+    var status = $("#status").val();
+    var maxcloud = $("#cloudRange").val();
+    var start = $("#startDate").val();
+    var end = $("#endDate").val();
+
+    if (scene && scene != "")
+      url = url + "name=" + scene + "&";
+    if (path && path != "")
+      url = url + "path=" + path + "&";
+    if (row && row != "")
+      url = url + "row=" + row + "&";
+    if (sat && sat != "")
+      url = url + "sat=" + sat + "&";
+    if (status && status != "")
+      url = url + "status=" + status + "&";
+    if ( maxcloud && maxcloud != "")
+      url = url + "max_cloud=" + maxcloud + "&";
+    if (start && start != "")
+      url = url + "start=" + start + "&";
+    if (end && end != "")
+      url = url + "end=" + end + "&";
+
+    url = url.substring(0,url.length - 1);
+    this.href = url;
+  });
+});
